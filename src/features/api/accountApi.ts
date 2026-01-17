534d3ce34b3af9faa830623858c13970c9b1039e
wargus666 <russianblue2181@gmail.com>
2026-01-17 13:33:50 +0200

    rtk-query

--- src/features/api/accountApi.ts
+++ src/features/api/accountApi.ts
@@ -1,98 +1,152 @@
-import {createAsyncThunk} from "@reduxjs/toolkit";
-import {base_url, createToken} from "../../utils/constants.ts";
+import {base_url} from "../../utils/constants.ts";
 import type {UserData, UserProfile, UserRegister} from "../../utils/types";
-import type {RootState} from "../../app/store.ts";
+import {createApi} from "@reduxjs/toolkit/query/react";
+import {fetchBaseQuery} from "@reduxjs/toolkit/query";
 
-export const registerUser = createAsyncThunk(
-    'user/register',
-    async (user: UserRegister) => {
-        const response = await fetch(`${base_url}/account/register`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
-        })
-        if (response.status === 409) {
-            throw new Error(`User ${user.login} already exists`);
-        }
-        if (!response.ok) {
-            throw new Error(`Failed to register user ${user.login}`);
-        }
-        const data = await response.json();
-        const token = createToken(user.login, user.password);
-        return {
-            user: data,
-            token
-        }
-    }
-)
-
-export const fetchUser = createAsyncThunk(
-    'user/fetch',
-    async (token: string) => {
-        const response = await fetch(`${base_url}/account/login`, {
-            method: 'POST',
-            headers: {
-                Authorization: token
-            }
-        });
-        if (response.status === 401) {
-            throw new Error(`Invalid credentials`);
-        }
-        if (!response.ok) {
-            throw new Error(`Something went wrong`);
-        }
-        const data = await response.json();
-        return {
-            user: data,
-            token
-        }
-    }
-)
-
-type UserUpdate = Omit<UserData, 'login'>
-
-export const updateUser = createAsyncThunk<UserProfile, UserUpdate, { state: RootState }>(
-    'user/update',
-    async (user, {getState}) => {
-        const response = await fetch(`${base_url}/account/user/${getState().user.login}`, {
-            method: 'PATCH',
-            headers: {
-                Authorization: `${getState().token}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
-        })
-        if (response.status === 401) {
-            throw new Error(`Invalid credentials`);
-        }
-        if (!response.ok) {
-            throw new Error(`Something went wrong`);
-        }
-        return await response.json();
-    }
-)
-
-export const changePassword = createAsyncThunk<string, {newPassword: string, oldPassword: string}, { state: RootState }>(
-    'user/password',
-    async ({newPassword, oldPassword}, {getState}) => {
-        const response = await fetch(`${base_url}/account/password`, {
-            method: 'PATCH',
-            headers: {
-                Authorization: createToken(getState().user.login, oldPassword),
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                password: newPassword
+export const accountApi = createApi({
+    reducerPath: 'account',
+    tagTypes: ['profile'],
+    baseQuery: fetchBaseQuery({baseUrl: base_url}),
+    endpoints: (builder) => ({
+        registerUser: builder.mutation<UserProfile, UserRegister>({
+            query: user => ({
+                url: '/account/register',
+                method: 'POST',
+                body: user
+            })
+        }),
+        fetchUser: builder.query<UserProfile, string>({
+            query: token => ({
+                url: '/account/login',
+                method: 'POST',
+                headers: {
+                    Authorization: token
+                }
+            }),
+            providesTags: ['profile']
+        }),
+        updateUser: builder.mutation<UserProfile, { login: string, token: string, user: Omit<UserData, 'login'> }>({
+            query: ({user, login, token}) => ({
+                url: `/account/user/${login}`,
+                method: 'PATCH',
+                body: user,
+                headers: {
+                    Authorization: token
+                }
+            }),
+            invalidatesTags: ['profile']
+        }),
+        changePassword: builder.mutation<void, { token: string, newPassword: string }>({
+            query: ({token, newPassword}) => ({
+                url: `/account/password`,
+                method: 'PATCH',
+                body: {password: newPassword},
+                headers: {
+                    Authorization: token
+                }
             })
         })
-        if (response.status === 401) {
-            throw new Error(`Invalid credentials`);
-        }
-        if (!response.ok) {
-            throw new Error(`Something went wrong`);
-        }
-        return createToken(getState().user.login, newPassword);
-    }
-)
+    })
+})
+
+export const {
+    useFetchUserQuery,
+    useLazyFetchUserQuery,
+    useUpdateUserMutation,
+    useChangePasswordMutation,
+    useRegisterUserMutation
+} = accountApi;
+
+// export const registerUser = createAsyncThunk(
+//     'user/register',
+//     async (user: UserRegister) => {
+//         const response = await fetch(`${base_url}/account/register`, {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(user)
+//         })
+//         if (response.status === 409) {
+//             throw new Error(`User ${user.login} already exists`);
+//         }
+//         if (!response.ok) {
+//             throw new Error(`Failed to register user ${user.login}`);
+//         }
+//         const data = await response.json();
+//         const token = createToken(user.login, user.password);
+//         return {
+//             user: data,
+//             token
+//         }
+//     }
+// )
+//
+// export const fetchUser = createAsyncThunk(
+//     'user/fetch',
+//     async (token: string) => {
+//         const response = await fetch(`${base_url}/account/login`, {
+//             method: 'POST',
+//             headers: {
+//                 Authorization: token
+//             }
+//         });
+//         if (response.status === 401) {
+//             throw new Error(`Invalid credentials`);
+//         }
+//         if (!response.ok) {
+//             throw new Error(`Something went wrong`);
+//         }
+//         const data = await response.json();
+//         return {
+//             user: data,
+//             token
+//         }
+//     }
+// )
+//
+// type UserUpdate = Omit<UserData, 'login'>
+//
+// export const updateUser = createAsyncThunk<UserProfile, UserUpdate, { state: RootState }>(
+//     'user/update',
+//     async (user, {getState}) => {
+//         const response = await fetch(`${base_url}/account/user/${getState().user.login}`, {
+//             method: 'PATCH',
+//             headers: {
+//                 Authorization: `${getState().token}`,
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(user)
+//         })
+//         if (response.status === 401) {
+//             throw new Error(`Invalid credentials`);
+//         }
+//         if (!response.ok) {
+//             throw new Error(`Something went wrong`);
+//         }
+//         return await response.json();
+//     }
+// )
+//
+// export const changePassword = createAsyncThunk<string, {newPassword: string, oldPassword: string}, { state: RootState }>(
+//     'user/password',
+//     async ({newPassword, oldPassword}, {getState}) => {
+//         const response = await fetch(`${base_url}/account/password`, {
+//             method: 'PATCH',
+//             headers: {
+//                 Authorization: createToken(getState().user.login, oldPassword),
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({
+//                 password: newPassword
+//             })
+//         })
+//         if (response.status === 401) {
+//             throw new Error(`Invalid credentials`);
+//         }
+//         if (!response.ok) {
+//             throw new Error(`Something went wrong`);
+//         }
+//         return createToken(getState().user.login, newPassword);
+//     }
+// )
